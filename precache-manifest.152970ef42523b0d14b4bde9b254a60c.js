918adb115dc8b7acce938efd348af099b6839d04
vamsi1133 <vamsiraju113@gmail.com>
2020-01-22 13:47:00 +0530

    add google ads in website

---
+++ precache-manifest.152970ef42523b0d14b4bde9b254a60c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abd466220003f6e45f75c158e53e9dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "899692e66c46a1765187",
+    "url": "./static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "372e203b5ff697c6b704",
+    "url": "./static/css/main.e03e65fe.chunk.css"
+  },
+  {
+    "revision": "899692e66c46a1765187",
+    "url": "./static/js/2.6ca499d4.chunk.js"
+  },
+  {
+    "revision": "18d92324f0e1cf937911489f8db23133",
+    "url": "./static/js/2.6ca499d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "372e203b5ff697c6b704",
+    "url": "./static/js/main.f0e294d3.chunk.js"
+  },
+  {
+    "revision": "228f027d83d837cea6af",
+    "url": "./static/js/runtime-main.3ba46a07.js"
+  },
+  {
+    "revision": "7708d739d4388efc3dbe0cdb532270a2",
+    "url": "./static/media/school1.7708d739.jpg"
+  }
+]);
